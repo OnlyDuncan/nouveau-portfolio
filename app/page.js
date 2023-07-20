c46f2fe94f22770b59c6c51f81f3154efcf9bb5a
Duncan Payne <Duncan.C.Payne@gmail.com>
2023-07-20 17:27:52 -0500

    Added Nav

--- app/page.js
+++ app/page.js
@@ -37,19 +37,41 @@ export default async function Home() {
               className="text-center text-white sourceCode" 
               style={{
                 fontSize: "50px",
-                letterSpacing: "0.075em"
+                letterSpacing: "0.07em"
               }}>
                 Duncan Payne
               </h1>
             </div>
             <div>
               <h2
-              className="text-center text-white acuminLight"
+              className="text-center text-white acuminLight mb-4"
               style={{
                 fontSize: "35px"
               }}>
                 Designer and Full-Stack Developer
               </h2>
+            </div>
+            <br />
+            <div id="navcontainer">
+              <nav>
+                <ul className="flex">
+                  <li className="inline ml-2 mr-2">
+                    <a href="#about" className="acuminMid text-white">About</a>
+                  </li>
+                  <li className="inline ml-2 mr-2">
+                    <a href="#design" className="acuminMid text-white">Design</a>
+                  </li>
+                  <li className="inline ml-2 mr-2">
+                    <a href="#projects" className="acuminMid text-white">Projects</a>
+                  </li>
+                  <li className="inline ml-2 mr-2">
+                    <a href="#skills" className="acuminMid text-white">Skills</a>
+                  </li>
+                  <li className="inline ml-2 mr-2">
+                    <a href="#contact" className="acuminMid text-white">Contact</a>
+                  </li>
+                </ul>
+              </nav>
             </div>
           </div>
         </header>

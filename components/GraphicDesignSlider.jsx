e1eaf65643730c6b70fb40360a472d3795b8c188
Duncan Payne <Duncan.C.Payne@gmail.com>
2023-08-05 13:06:00 -0500

    Cleaning Code

--- components/GraphicDesignSlider.jsx
+++ components/GraphicDesignSlider.jsx
@@ -211,24 +211,6 @@ const GraphicDesignSlider = () => {
                     )}
                 </div>
             </div>
-
-            {loaded && instanceRef.current && (
-                <div className="dots">
-                    {[
-                        ...Array(instanceRef.current.track.details.slides.length).keys(),
-                    ].map((idx) => {
-                        return (
-                            <button
-                                key={idx}
-                                onClick={() => {
-                                    instanceRef.current?.moveToIdx(idx)
-                                }}
-                                className={"dot" + (currentSlide === idx ? " active" : "")}
-                            ></button>
-                        )
-                    })}
-                </div>
-            )}
         </>
     )
 }
--- components/UiUxSlider.jsx
+++ components/UiUxSlider.jsx
@@ -91,24 +91,6 @@ const UiUxSlider = () => {
                     )}
                 </div>
             </div>
-
-            {loaded && instanceRef.current && (
-                <div className="dots">
-                    {[
-                        ...Array(instanceRef.current.track.details.slides.length).keys(),
-                    ].map((idx) => {
-                        return (
-                            <button
-                                key={idx}
-                                onClick={() => {
-                                    instanceRef.current?.moveToIdx(idx)
-                                }}
-                                className={"dot" + (currentSlide === idx ? " active" : "")}
-                            ></button>
-                        )
-                    })}
-                </div>
-            )}
         </>
     )
 }
